dc0f63b04241d790a85f4f972203161b54b6c4f1
you-kimono <savioli.enrico@gmail.com>
2018-08-17 17:40:16 +0200

    Add template for home_page and test it

--- goyellow/enterprises/tests.py
+++ goyellow/enterprises/tests.py
@@ -8,3 +8,7 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_uses_correct_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'enterprises/home.html')
--- goyellow/enterprises/views.py
+++ goyellow/enterprises/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+
 # Create your views here.
-def home_page():
-    pass
+def home_page(request):
+    return render(request, 'enterprises/home.html')
