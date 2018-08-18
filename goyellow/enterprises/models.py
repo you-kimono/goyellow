a2f0001fe01b6da90d1b3642588eea1bb3a97748
you-kimono <savioli.enrico@gmail.com>
2018-08-18 09:34:33 +0200

    Add Enterprise model with attribute name

--- goyellow/enterprises/models.py
+++ goyellow/enterprises/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Enterprise(models.Model):
+    name = models.CharField(
+        max_length=255,
+        default=''
+    )
--- goyellow/enterprises/tests/test_models.py
+++ goyellow/enterprises/tests/test_models.py
@@ -1,6 +1,23 @@
-from django.urls import resolve
 from django.test import TestCase
-from enterprises.views import home_page
-from enterprises.views import details
+from ..models import Enterprise
 
 # Create your tests here.
+class EnterpriseModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_enterprise = Enterprise()
+        first_enterprise.name = 'first_enterprise'
+        first_enterprise.save()
+
+        second_enterprise = Enterprise()
+        second_enterprise.name = 'second_enterprise'
+        second_enterprise.save()
+
+        saved_enterprises = Enterprise.objects.all()
+        self.assertEqual(saved_enterprises.count(), 2)
+
+        first_saved_enterprise = saved_enterprises[0]
+        second_saved_enterprise = saved_enterprises[1]
+
+        self.assertEqual(first_saved_enterprise.name, 'first_enterprise')
+        self.assertEqual(second_saved_enterprise.name, 'second_enterprise')
