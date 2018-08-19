b602b79321a6a1a20b0db1b21fb76046b1231236
you-kimono <savioli.enrico@gmail.com>
2018-08-19 22:10:50 +0200

    Add feature for add enterprises using the form, to cover view code 100%

--- goyellow/features/enterprises_new.feature
+++ goyellow/features/enterprises_new.feature
@@ -4,3 +4,9 @@ Feature: Add a new enterprise.
     Given I am logged in
     when I access the create new enterprise page
     then I see a form where I can enter the enterprise info
+
+  Scenario: A user creates a new enterprise page
+    Given I am logged in
+    when I access the create new enterprise page
+    and I compile the form with name "new_enterprise" and press submit
+    then the a new enterprise with name "new_enterprise" is created
--- goyellow/features/steps/enterprises_new.py
+++ goyellow/features/steps/enterprises_new.py
@@ -1,5 +1,7 @@
 from behave import *
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from enterprises.models import Enterprise
 import time
 
 
@@ -20,3 +22,18 @@ def step_impl(context):
     time.sleep(1)
     element = context.browser.find_element(By.ID, 'new_enterprise_form')
     assert element is not None
+
+
+@when('I compile the form with name "{enterprise_name}" and press submit')
+def step_impl(context, enterprise_name):
+    time.sleep(1)
+    element = context.browser.find_element(By.ID, 'id_enterprise_name')
+    element.send_keys(enterprise_name)
+    element.send_keys(Keys.RETURN)
+
+
+@then('the a new enterprise with name "{enterprise_name}" is created')
+def step_impl(context, enterprise_name):
+    time.sleep(1)
+    enterprise = Enterprise.objects.first()
+    context.test.assertEqual(enterprise.enterprise_name, enterprise_name)
